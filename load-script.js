223d9fcdc968b9544e47c28791974485f7595cbe
narushevich-blockvis <denis.narushevich@blockvis.com>
2024-02-15 15:22:39 +0200

    init

---
+++ load-script.js
@@ -0,0 +1,19 @@
+"use strict";
+/* eslint-disable */
+// Loads all inline scripts
+
+// Google tag (gtag.js)
+// Uncomment the script bellow and update with correct ID
+
+// (function (w, d, s, l, i, p) {
+//     w[l] = w[l] || [];
+//     function gtag(){w[l].push(arguments)}
+//     w["gtag"] = gtag;
+//     gtag('js', new Date());
+//     gtag('config', i, p);
+//     const f = d.getElementsByTagName(s)[0];
+//     const j = d.createElement(s);
+//     j.async = true;
+//     j.src = 'https://www.googletagmanager.com/gtag/js?id=' + i;
+//     f.parentNode.insertBefore(j, f);
+// })(window, document, 'script', 'dataLayer', 'G-XXXXXXXXXX', { debug_mode: false });
